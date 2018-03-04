ad279729ecab17eab28b348d92c93ecf0cda5df8
Oleg Demus <oleguk97@gmail.com>
2018-03-04 12:38:08 -0500

    Updates

--- Lab05Try/src/URArrayList.java
+++ Lab05Try/src/URArrayList.java
@@ -1,18 +1,41 @@
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 
-public class URArrayList<E> implements URList<E>{
+public class URArrayList<E> implements URList<E> {
+
+	private int size;
+	private static int capacity = 10;
+	private Object[] data;;
+
+	public URArrayList() {
+		data = new Object[capacity];
+		size = 0;
+	}
 
 	@Override
 	public boolean add(Object e) {
-		// TODO Auto-generated method stub
-		return false;
+		add(size, e);
+		return true;
 	}
 
 	@Override
 	public void add(int index, Object element) {
-		// TODO Auto-generated method stub
-		
+
+		if (index < 0 || index > size) {
+			throw new ArrayIndexOutOfBoundsException(index);
+		}
+		if (element == null) {
+			throw new NullPointerException();
+		}
+
+		ensureCapacity(size);
+		for (int i = size; i > index; i--) {
+			data[i] = data[i - 1];
+		}
+		data[index] = element;
+		size++;
+
 	}
 
 	@Override
@@ -29,13 +52,18 @@ public class URArrayList<E> implements URList<E>{
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		data = new Object[capacity];
+		capacity = 10;
+		size = 0;
 	}
 
 	@Override
 	public boolean contains(Object o) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i <= size; i++) {
+			if (data[i] == o) {
+				return true;
+			}
+		}
 		return false;
 	}
 
@@ -46,21 +74,29 @@ public class URArrayList<E> implements URList<E>{
 	}
 
 	@Override
-	public Object get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+	public E get(int index) {
+		if (index < 0 || index >= size) {
+			throw new RuntimeException("OUT OF BOUNDS");
+		}
+		return (E) data[index];
 	}
 
 	@Override
 	public int indexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+		if (o == null) {
+			throw new NullPointerException();
+		}
+		for (int i = 0; i <= size; i++) {
+			if (data[i] == o) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return size == 0;
 	}
 
 	@Override
@@ -70,14 +106,27 @@ public class URArrayList<E> implements URList<E>{
 	}
 
 	@Override
-	public Object remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+	public E remove(int index) {
+		if (index < 0 || index > size) {
+			throw new ArrayIndexOutOfBoundsException(index);
+		}
+		E removed = (E) data[index];
+		for (int i = index + 1; i < size; i++) {
+			data[i - 1] = data[i];
+		}
+		size--;
+		return removed;
 	}
 
 	@Override
 	public boolean remove(Object o) {
-		// TODO Auto-generated method stub
+		if (o == null) {
+			throw new NullPointerException();
+		}
+		if (indexOf(o) == -1) {
+			return false;
+		}
+		remove(indexOf(o));
 		return false;
 	}
 
@@ -95,8 +144,7 @@ public class URArrayList<E> implements URList<E>{
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
 	@Override
@@ -110,17 +158,26 @@ public class URArrayList<E> implements URList<E>{
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	// Increases the capacity of this ArrayList instance, if necessary,
 	// to ensure that it can hold at least the number of elements specified
 	// by the minimum capacity argument.
 	public void ensureCapacity(int minCapacity) {
-		
+		if (minCapacity == capacity) {
+			capacity = capacity * 2;
+			Object tempData[] = new Object[capacity];
+			// Copy old array into new array
+			for (int i = 0; i < size; i++) {
+				tempData[i] = data[i];
+			}
+			data = tempData;
+		}
 	}
+
 	// Returns the current capacity of the list
 	public int getCapacity() {
-		return 0;
-		
+		return capacity;
+
 	}
-	
-	
+
 }
